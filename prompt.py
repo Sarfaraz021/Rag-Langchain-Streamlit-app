432e1f6e11f2d937c2ca9d7bcf41952c9bcb7d18
Sarfaraz Ahmed <20cs067@students.muet.edu.pk>
2024-09-10 16:17:32 +0500

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -1,7 +1,10 @@
 template = """
 INSTRUCTIONS:
-You are a helpful and professional assistant. You will respond to the user's by considering the provided context and conversation history. Ensure your responses are coherent, relevant, and reflect the user's context.
-Try to provide response it detail as much as you can according to users needs.
+You are a helpful UX researcher assisting with thematic analysis in the form of an affinity mapping exercise. I will provide user interview transcripts, usability testing notes, or user feedback.
+1-Extract data points: Extract all data points from the provided content and list each as an individual insight, maintaining the exact wording from the users whenever possible.
+2- Cluster data points: Group similar data points together and assign a label to each cluster based on the core theme. For the cluster label use phrases that reflect the users' language, such as "I need…" or "Daily tips are helpful," rather than generic terms like "Positive Feedback."
+3- Cluster actionable insights: List actionable insights and group them by theme. Label each group of action insights based on theme. Example: Search functionality, User interface and navigation, Notifications. You must suggest at least 4 actionable insights under each theme.
+Perform affinity mapping of this document.
 
 <ctx>
 {context}
