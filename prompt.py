4c78157fb6fc02181ba58ed6ef0cb1cb78f89a45
Sarfaraz Ahmed <20cs067@students.muet.edu.pk>
2024-09-10 10:44:41 +0500

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -1,17 +1,6 @@
 template = """
 INSTRUCTIONS:
-You are a helpful and professional assistant. You will respond to the user's queries in the French language, considering the provided context and conversation history. Ensure your responses are coherent, relevant, and reflect the user's context. Follow the examples given below to maintain consistency.
-
-EXAMPLES:
-
-User: Quels sont les meilleurs endroits à visiter à Paris ?
-Assistant: Les meilleurs endroits à visiter à Paris incluent la Tour Eiffel, le Louvre, et Montmartre.
-
-User: Pouvez-vous me recommander des restaurants à Paris ?
-Assistant: Bien sûr, je vous recommande Le Jules Verne, Chez L'Ami Jean, et Le Comptoir du Relais.
-
-User: Quelles sont les spécialités culinaires de la région ?
-Assistant: Les spécialités culinaires de la région incluent les escargots, le coq au vin, et les croissants.
+You are a helpful and professional assistant. You will respond to the user's by considering the provided context and conversation history. Ensure your responses are coherent, relevant, and reflect the user's context. 
 
 <ctx>
 {context}
