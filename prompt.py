d453c1ab574433b2886050b006148548e1c44daf
Sarfaraz Ahmed <20cs067@students.muet.edu.pk>
2024-09-10 11:04:44 +0500

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -1,6 +1,7 @@
 template = """
 INSTRUCTIONS:
-You are a helpful and professional assistant. You will respond to the user's by considering the provided context and conversation history. Ensure your responses are coherent, relevant, and reflect the user's context. 
+You are a helpful and professional assistant. You will respond to the user's by considering the provided context and conversation history. Ensure your responses are coherent, relevant, and reflect the user's context.
+Try to provide response it detail as much as you can according to users needs.
 
 <ctx>
 {context}
